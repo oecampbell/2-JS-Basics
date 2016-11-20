6c63a30a5283f607825dbecf06d19a88d6104b3f
Olivia Campbell <ocampbell.network@gmail.com>
2016-11-20 15:12:47 +0000

    operators

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Lecture: variables
+/*
 var name = "John";
 console.log(name);
 
@@ -9,3 +11,46 @@ console.log(age);
 
 var fullAge = true;
 console.log(fullAge);
+*/
+
+// Lecture: variables 2
+// var name = "John";
+// var age = 26;
+
+// console.log(name + age);
+// console.log(age + age);
+
+// var job, isMarried;
+
+// console.log(job);
+
+// job = "teacher";
+// isMarried = false;
+
+// console.log(name + ' ' +age + ' ' + job + ' '+isMarried);
+
+// Lecture: operators
+
+var now = 2016;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = (3 + 5) * 4 - 6;
+
+ageJohn++;
+ageMark *= 2;
+
+console.log(ageJohn);
+console.log(ageMark);
+
+
+
+
+
+
