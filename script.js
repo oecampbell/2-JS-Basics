dda33cdf7bc4890e83dbd50a1e7e76741c6ea838
Olivia Campbell <ocampbell.network@gmail.com>
2016-11-24 21:57:10 +0000

    next lecture

--- script.js
+++ script.js
@@ -67,6 +67,34 @@ console.log(fullAge);
 //   console.log("YES!");
 // }
 
+// Lecture: boolean logic and switch
+var age = 25;
+
+if (age < 20){
+    console.log("John is a teenager");
+} else if (age > 20 && age <30) {
+    console.log("John is a young man.")
+} else {
+    console.log("John is a man.")
+}
+
+
+var job = "teacher"
+
+job = prompt("What does John do?")
+
+switch (job){
+  case "teacher":
+    console.log("John teaches kids.");
+    break;
+  case "driver":
+    console.log("John drives a cab.")
+    break;
+  default:
+    console.log("John does something else.");
+}
+
+
 
 
 
